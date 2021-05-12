dd9c2a36c27f2433203c6f28b2be1adf0f388ac3
omerturkgeldi <turkgeldiomer@gmail.com>
2021-05-12 13:57:51 +0300

    "Adapters" Updated

--- coffeeShopHW/src/coffeeShopHW/Main.java
+++ coffeeShopHW/src/coffeeShopHW/Main.java
@@ -2,17 +2,20 @@ package coffeeShopHW;
 
 import java.util.Date;
 
-import Adapters.MernisServiceAdapter;
+import coffeeShopHW.Adapters.*;
 import coffeeShopHW.Abstract.BaseCustomerManager;
 import coffeeShopHW.Concrete.*;
 import coffeeShopHW.Entities.Customer;
 
+
+
 public class Main {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 
 
-		BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+		BaseCustomerManager customerManager = new NeroCustomerManager(new coffeeShopHW.Adapters.MernisServiceAdapter());
 		customerManager.save(new Customer(1, "Gazi Mustafa Kemal", "Atatürk", new Date(1881, 07, 01), "10000000146"));
 		
 	}
